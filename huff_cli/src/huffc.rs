58dd307197b40372b8324b580d60b686f586865b
shafu.eth <selfouly@gmail.com>
2023-10-17 16:06:42 +0300

    refactor

--- huff_cli/src/huffc.rs
+++ huff_cli/src/huffc.rs
@@ -15,62 +15,6 @@ mod opts;
 mod formatter;
 use formatter::Formatter;
 
-fn fmt_include(generator: &Generator, formatted: &mut String) {
-    if let TokenKind::Str(string) = &generator.peeks(0).unwrap().kind {
-        formatted.push_str(&format!("#include \"{}\"", &string));
-    }
-}
-
-fn fmt_constant(generator: &mut Generator, formatted: &mut String, token: &Token) {
-    if let TokenKind::Ident(ident) = &generator.peeks(1).unwrap().kind {
-        if let TokenKind::Num(num) = &generator.peeks(3).unwrap().kind {
-            formatted.push_str(&format!("{} constant {} = {}", token.kind, ident, num));
-            generator.increment_index(4);
-        }
-    }
-}
-
-fn fmt_code_table(generator: &mut Generator, formatted: &mut String, token: &Token) {
-    if let TokenKind::Ident(ident) = &generator.peeks(1).unwrap().kind {
-        if let TokenKind::Ident(ident2) = &generator.peeks(3).unwrap().kind {
-            formatted.push_str(&format!("{} table {} {{ \n  {}}}", &token.kind, ident, ident2));
-            generator.increment_index(4);
-        }
-    }
-}
-
-fn fmt_macro(generator: &mut Generator, formatted: &mut String, token: &Token) {
-    // start of macro
-    if let TokenKind::Ident(ident) = &generator.peeks(1).unwrap().kind {
-        let takes = &generator.peeks(7).unwrap().kind;
-        let returns = &generator.peeks(11).unwrap().kind;
-
-        formatted.push_str(&format!(
-            "#define macro {} = takes({}) returns({}) {{",
-            ident, takes, returns,
-        ));
-
-        generator.increment_index(14);
-    }
-
-    // in macro
-    while generator.peeks(0).unwrap().kind != TokenKind::CloseBrace {
-        let token = generator.next().unwrap();
-        // check_new_line(token.line_number, &mut formatted);
-        match token.kind {
-            TokenKind::Opcode(opcode) => {
-                formatted.push_str(&format!("    {}", opcode.format()));
-            }
-            _ => {
-                formatted.push_str(&format!("    {}", token.kind.to_string()));
-            }
-        }
-    }
-
-    // end of macro
-    formatted.push_str("\n}}");
-}
-
 fn main() {
     // parsing args
     let opts = Opts::parse();
